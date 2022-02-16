842321da538c69b0c5caa586260650e39e7f2cd3
Sergei Seliverstov <seliverstovsk@gmail.com>
2022-02-16 22:20:04 +0300

    delete 'List<MenuItem> menu' property from Restaurant entity

--- src/main/java/ru/ssk/restvoting/model/Restaurant.java
+++ src/main/java/ru/ssk/restvoting/model/Restaurant.java
@@ -1,6 +1,5 @@
 package ru.ssk.restvoting.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.annotations.Cache;
@@ -9,7 +8,6 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 @Entity
 @Table(name = "restaurants")
@@ -22,20 +20,6 @@ public class Restaurant extends AbstractEmailEntity {
     @Column(name = "address")
     @ApiModelProperty(notes = "The restaurant address. Must be unique.")
     private String address;
-
-    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "restaurant")
-    @JsonIgnore
-    @ApiModelProperty(notes = "The list of menu items in restaurant menu.")
-    private List<MenuItem> menu;
-
-    public List<MenuItem> getMenu() {
-        return menu;
-    }
-
-    public void setMenu(List<MenuItem> menu) {
-        this.menu = menu;
-    }
 
     public String getAddress() {
         return address;
